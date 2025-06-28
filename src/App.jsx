26455c30c657674fa939c92fa59a95ae09f188ad
RaviCoderWallah <raviverma05042023@gmail.com>
2025-06-28 11:39:56 +0530

    Routing Implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { NavLink, Outlet } from 'react-router-dom'
 import './App.css'
+import SideBar from './components/SideBar'
 
 function App() {
   return (
    <>
-     <div className='text-4xl'>
-        <h1>Hello</h1>
-     </div>
+     <SideBar/>
+     <Outlet/>
    </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Budgets from './Pages/Budgets.jsx';
+import Pots from './Pages/Pots.jsx';
+import Transactions from './Pages/Transactions.jsx';
+import RecurringBills from './Pages/RecurringBills.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/budgets",
+        element: <Budgets />
+      },
+      {
+        path: "/pots",
+        element: <Pots />
+      },
+      {
+        path: "/transactions",
+        element: <Transactions/>
+      },
+            {
+        path: "/recurring",
+        element: <RecurringBills/>
+      },
+    ],
+  },
+]);
+
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
